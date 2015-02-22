1d77e0883c7e8de2d4e552d68fc3da7c2f84ef7f
FrozenImpact <laimis94@hotmail.com>
2015-02-22 22:53:43 +0200

    asd

--- index.php
+++ index.php
@@ -1,20 +1,28 @@
-<html>
-
-<head>
 <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
 <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
-</head>
 
-<?php
+<h1>Matemaatiline analüüs</h1><br/>
 
-	echo '<h1>Matemaatiline analüüs</h1>';
-
-	echo '<a class="s1" href="#">post</a><br />';
-	echo '<a class="s1" href="#">post</a><br />';
-	echo '<a class="s1" href="#">post</a><br />';
-	echo '<a class="s1" href="#">post</a><br />';
-	echo '<a class="s1" href="#">post</a><br />';	
-
-?>
-
-</html>
+<div class="center">
+	
+	<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" />
+	<a class="s1" href="#">Kas kellgil on esimese kodutöö näidist?
+	<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt="" /><br/><br/>
+	
+	<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" />
+	<a class="s1" href="#">Millal meil kollokvium tuleb?
+	<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt="" /><br/><br/>
+	
+	<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" />
+	<a class="s1" href="#">See õppejõud on ikka päris tolvan
+	<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt="" /><br/><br/>
+	
+	<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" />
+	<a class="s1" href="#">Ma ei saa midagi aru
+	<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt="" /><br/><br/>
+	
+	<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" />
+	<a class="s1" href="#">Kus Liivi maja asub üldse :o
+	<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt="" /><br/><br/>
+  
+</div>
