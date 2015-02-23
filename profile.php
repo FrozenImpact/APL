3571ed58cb1b824ae2c17c263783243f583bd30f
anniu <anniuut@gmail.com>
2015-02-23 02:29:33 +0200

    asetus on paigast ära natuke, hiljem tegelen edasi

---
+++ profile.php
@@ -0,0 +1,39 @@
+<html>
+
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+<link rel="stylesheet" type="text/css" href="profilestyle.css" media="screen" />
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400;300' rel='stylesheet' type='text/css'>
+</head>
+
+		<table id= 'image'>
+			<tr>
+				<td><img src='http://upload.wikimedia.org/wikipedia/commons/1/12/Akhenaten_skull_profile.png'></td>
+			</tr>
+			<tr>
+				<td style='text-align: center' class='userinfo'> <?php echo "Joined: " . date("Y/m/d"); ?></td>
+			</tr>
+			<tr>
+				<td style='text-align: center'><a href='http://www.gmail.com/'><img src='http://www.for-sale-businesses.com/public/images/send_messege.gif' style='width:150px;height:35px'>
+			</tr>
+		</table>
+		<div class='userinfo' id='meh'>
+			<div id='username'>YUNoEnterUsername</div><br>
+			Posts: 34<br>
+			Comments: 105<br>
+
+			<section class='timeline' href="style.css">
+				<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="20" height="20" alt="" />
+
+				<a>midamida</a>
+			
+				<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="downV.png" width="20" height="20" alt="" />
+				
+				<br /><a>mehs</a>
+				<a>asd123</a>
+				<a>dg</a>
+				<br /><br />
+			</section>
+
+		</div>
+</html>
