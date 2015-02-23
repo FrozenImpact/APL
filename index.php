30bf3a9e9ee866d026006c4b364e57f0fb19c28b
FrozenImpact <laimis94@hotmail.com>
2015-02-23 18:32:38 +0200

    fixed the button maddness and cleaned up come

--- index.php
+++ index.php
@@ -1,35 +1,38 @@
 <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
 <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
 
-
-
-<div class="container">
+	
+	
 	<div class="right">
 		<div class="up">
 			<div class="upUp">
 				<div class="upUpLeft">	
+				
 					<a href="http://www.reddit.com/"><img class="pic" src="pic.jpeg" width="100" height="100"/></a><br/><br/>
 					<h>Joined: 29.01.2015</h><br/><br/>
 					<h>Last Visit: 29.01.2015</h><br/><br/>
+					
 				</div>
 				<div class="upUpRight">	
-					<a class="panRight" href="#1">PotatoeMan</a><br/><br/>
-					<a class="panRight" href="#2">Posts: 54</a><br/><br/>
-					<a class="panRight" href="#3">Comments: 172</a><br/><br/>
+				
+					<br/><a class="n1" href="#1">PotatoeMan</a><br/><br/>
+					<a class="n1" href="#2">Posts: 54</a><br/><br/>
+					<a class="n1" href="#3">Comments: 172</a><br/><br/>
+					
 				</div>
 			</div>
 			<div class="upDown">
-				<form action="http://google.com">
-					<input class="butLeft" type="submit" value="Settings">
-				</form>
-				<form action="http://google.com">
-					<input class="butRight" type="submit" value="Log Out">
-				</form>
+			
+				<a href="https://reddit.com"><button type="button">Settings</button></a>
+				<a href="https://reddit.com"><button type="button">Log Out</button></a>
+				
 			</div>
 		</div>
 		<div class="down">					
 		</div>
     </div>
+	
+	
     <div class="left">
 		
 		<h1>Matemaatiline analüüs</h1><br/><br/>
