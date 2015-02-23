b3471dd870a4faa02710288f9dddd2023875acfa
FrozenImpact <laimis94@hotmail.com>
2015-02-23 12:03:58 +0200

    tegin natuke edasi

--- index.php
+++ index.php
@@ -6,33 +6,53 @@
 <div class="container">
 	<div class="right">
 		<div class="up">
+			<div class="upUp">
+				<div class="upUpLeft">	
+					<a href="http://www.reddit.com/"><img class="pic" src="pic.jpeg" width="100" height="100"/></a><br/><br/>
+					<h>Joined: 29.01.2015</h><br/><br/>
+					<h>Last Visit: 29.01.2015</h><br/><br/>
+				</div>
+				<div class="upUpRight">	
+					<a class="panRight" href="#1">PotatoeMan</a><br/><br/>
+					<a class="panRight" href="#2">Posts: 54</a><br/><br/>
+					<a class="panRight" href="#3">Comments: 172</a><br/><br/>
+				</div>
+			</div>
+			<div class="upDown">
+				<form action="http://google.com">
+					<input class="butLeft" type="submit" value="Settings">
+				</form>
+				<form action="http://google.com">
+					<input class="butRight" type="submit" value="Log Out">
+				</form>
+			</div>
 		</div>
-		<div class="down">
+		<div class="down">					
 		</div>
     </div>
     <div class="left">
 		
 		<h1>Matemaatiline analüüs</h1><br/><br/>
 		
-		<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" />
-		<a class="s1" href="#">Kas kellelgi on esimese kodutöö näidist?
-		<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt="" /><br/><br/>
+		<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" /></a>
+		<a class="s1" href="#4">Kas kellelgi on esimese kodutöö näidist?</a>
+		<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt=""/></a><br/><br/>
 		
-		<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" />
-		<a class="s1" href="#">Millal meil kollokvium tuleb?
-		<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt="" /><br/><br/>
+		<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" /></a>
+		<a class="s1" href="#5">Millal meil kollokvium tuleb?</a>
+		<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt="" /></a><br/><br/>
 		
-		<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" />
-		<a class="s1" href="#">See õppejõud on ikka päris tolvan
-		<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt="" /><br/><br/>
+		<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" /></a>
+		<a class="s1" href="#6">See õppejõud on ikka päris tolvan</a>
+		<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt="" /></a><br/><br/>
 		
-		<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" />
-		<a class="s1" href="#">Ma ei saa midagi aru
-		<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt="" /><br/><br/>
+		<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" /></a>
+		<a class="s1" href="#7">Ma ei saa midagi aru</a>
+		<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt="" /></a><br/><br/>
 		
-		<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" />
-		<a class="s1" href="#">Kus Liivi maja asub üldse :o
-		<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt="" /><br/><br/>
+		<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" /></a>
+		<a class="s1" href="#8">Kus Liivi maja üldse asub :o</a>
+		<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt="" /></a><br/><br/>
 	  
 	</div>	   
 </div>
