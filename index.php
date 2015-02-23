25b6e75b00fbe3b5b7d8a7ac213d082b7dfb1310
FrozenImpact <laimis94@hotmail.com>
2015-02-23 18:34:50 +0200

    fixed the button maddness and cleaned up come

--- index.php
+++ index.php
@@ -3,59 +3,56 @@
 
 	
 	
-	<div class="right">
-		<div class="up">
-			<div class="upUp">
-				<div class="upUpLeft">	
-				
-					<a href="http://www.reddit.com/"><img class="pic" src="pic.jpeg" width="100" height="100"/></a><br/><br/>
-					<h>Joined: 29.01.2015</h><br/><br/>
-					<h>Last Visit: 29.01.2015</h><br/><br/>
-					
-				</div>
-				<div class="upUpRight">	
-				
-					<br/><a class="n1" href="#1">PotatoeMan</a><br/><br/>
-					<a class="n1" href="#2">Posts: 54</a><br/><br/>
-					<a class="n1" href="#3">Comments: 172</a><br/><br/>
-					
-				</div>
-			</div>
-			<div class="upDown">
+<div class="right">
+	<div class="up">
+		<div class="upUp">
+			<div class="upUpLeft">	
 			
-				<a href="https://reddit.com"><button type="button">Settings</button></a>
-				<a href="https://reddit.com"><button type="button">Log Out</button></a>
+				<a href="http://www.reddit.com/"><img class="pic" src="pic.jpeg" width="100" height="100"/></a><br/><br/>
+				<h>Joined: 29.01.2015</h><br/><br/>
+				<h>Last Visit: 29.01.2015</h><br/><br/>
+				
+			</div>
+			<div class="upUpRight">	
+			
+				<br/><a class="n1" href="#1">PotatoeMan</a><br/><br/>
+				<a class="n1" href="#2">Posts: 54</a><br/><br/>
+				<a class="n1" href="#3">Comments: 172</a><br/><br/>
 				
 			</div>
 		</div>
-		<div class="down">					
+		<div class="upDown">
+		
+			<a href="https://reddit.com"><button type="button">Settings</button></a>
+			<a href="https://reddit.com"><button type="button">Log Out</button></a>
+			
 		</div>
-    </div>
+	</div>
+	<div class="down">					
+	</div>
+</div>	
+<div class="left">
 	
+	<h1>Matemaatiline analüüs</h1><br/><br/>
 	
-    <div class="left">
-		
-		<h1>Matemaatiline analüüs</h1><br/><br/>
-		
-		<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" /></a>
-		<a class="s1" href="#4">Kas kellelgi on esimese kodutöö näidist?</a>
-		<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt=""/></a><br/><br/>
-		
-		<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" /></a>
-		<a class="s1" href="#5">Millal meil kollokvium tuleb?</a>
-		<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt="" /></a><br/><br/>
-		
-		<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" /></a>
-		<a class="s1" href="#6">See õppejõud on ikka päris tolvan</a>
-		<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt="" /></a><br/><br/>
-		
-		<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" /></a>
-		<a class="s1" href="#7">Ma ei saa midagi aru</a>
-		<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt="" /></a><br/><br/>
-		
-		<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" /></a>
-		<a class="s1" href="#8">Kus Liivi maja üldse asub :o</a>
-		<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt="" /></a><br/><br/>
-	  
-	</div>	   
+	<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" /></a>
+	<a class="s1" href="#4">Kas kellelgi on esimese kodutöö näidist?</a>
+	<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt=""/></a><br/><br/>
+	
+	<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" /></a>
+	<a class="s1" href="#5">Millal meil kollokvium tuleb?</a>
+	<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt="" /></a><br/><br/>
+	
+	<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" /></a>
+	<a class="s1" href="#6">See õppejõud on ikka päris tolvan</a>
+	<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt="" /></a><br/><br/>
+	
+	<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" /></a>
+	<a class="s1" href="#7">Ma ei saa midagi aru</a>
+	<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt="" /></a><br/><br/>
+	
+	<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" /></a>
+	<a class="s1" href="#8">Kus Liivi maja üldse asub :o</a>
+	<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt="" /></a><br/><br/>
+  
 </div>
