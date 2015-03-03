705ebb3d5686a63628516a0140adf8f13c2025d2
FrozenImpact <Laimis>
2015-03-03 12:41:03 +0200

    postituste vaade pealehel

--- index.php
+++ index.php
@@ -6,23 +6,20 @@
 <div class="right">
 	<div class="up">
 		<div class="upUp">
-			<div class="upUpLeft">	
-			
+			<div class="upUpLeft">			
 				<a href="http://www.reddit.com/"><img class="pic" src="pic.jpeg" width="100" height="100"/></a><br/><br/>
 				<h>Joined: 29.01.2015</h><br/><br/>
 				<h>Last Visit: 29.01.2015</h><br/><br/>
 				
 			</div>
-			<div class="upUpRight">	
-			
+			<div class="upUpRight">				
 				<br/><a class="n1" href="#1">PotatoeMan</a><br/><br/>
 				<a class="n1" href="#2">Posts: 54</a><br/><br/>
 				<a class="n1" href="#3">Comments: 172</a><br/><br/>
 				
 			</div>
 		</div>
-		<div class="upDown">
-		
+		<div class="upDown">		
 			<a href="https://reddit.com"><button type="button">Settings</button></a>
 			<a href="https://reddit.com"><button type="button">Log Out</button></a>
 			
@@ -31,28 +28,34 @@
 	<div class="down">					
 	</div>
 </div>	
-<div class="left">
-	
+
+<div class="left">	
 	<h1>Matemaatiline analüüs</h1><br/><br/>
 	
-	<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" /></a>
-	<a class="s1" href="#4">Kas kellelgi on esimese kodutöö näidist?</a>
-	<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt=""/></a><br/><br/>
+	<div class="postBox">	
+		<div class="voteBox">
+			<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" /></a>
+		</div>
+		<div class="postDataBox">	
+			<div class="postDataBoxUp">	
+				<a class="s1" href="#4">Kas asdasdasdasdadasdasasdaskest?</a>
+			</div>
+			<div class="postDataBoxDown">
+				<div class="dataComments">	
+					<h>Replies: 16</h>
+				</div>
+				<div class="dataScore">	
+					<h>78</h>
+				</div>				
+				<div class="dataDate">	
+					<h>16.02.2015</h>
+				</div>
+			</div>			
+		</div>			
+		<div class="voteBox">
+			<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt=""/></a>
+		</div>
+				
+	</div>
 	
-	<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" /></a>
-	<a class="s1" href="#5">Millal meil kollokvium tuleb?</a>
-	<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt="" /></a><br/><br/>
-	
-	<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" /></a>
-	<a class="s1" href="#6">See õppejõud on ikka päris tolvan</a>
-	<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt="" /></a><br/><br/>
-	
-	<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" /></a>
-	<a class="s1" href="#7">Ma ei saa midagi aru</a>
-	<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt="" /></a><br/><br/>
-	
-	<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="upV.png" width="50" height="50" alt="" /></a>
-	<a class="s1" href="#8">Kus Liivi maja üldse asub :o</a>
-	<a class="s1" href="http://www.reddit.com/"><img class="mid" src="downV.png" width="50" height="50" alt="" /></a><br/><br/>
-  
 </div>
