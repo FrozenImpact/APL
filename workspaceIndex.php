23fd14941d2392827b0d635d7884e7cb99c30a9d
FrozenImpact <laimis94@hotmail.com>
2015-03-12 18:37:51 +0200

    index page is coming

--- workspaceIndex.php
+++ workspaceIndex.php
@@ -20,20 +20,165 @@
 			</div>
 		</div>
 		<div class="upDown">		
-			<a href="https://reddit.com"><button type="button">Settings</button></a>
-			<a href="https://reddit.com"><button type="button">Log Out</button></a>
-			
+			<a href="https://reddit.com"><button class="settingsBut" type="button">Settings</button></a>
+			<a href="https://reddit.com"><button class="logOutBut" type="button">Log Out</button></a>			
 		</div>
 	</div>
-	<div class="down">					
+	<div class="down">
+	asd
 	</div>
 </div>	
 
 <div class="left">	
-	<div class="head">
-		<j1>Matemaatiline analüüs</j1>
-		
+	<div class="head">	
+		<div class="headLeft">
+			<a class="midLink">APL</a>
+		</div>
+		<div class="headMid">
+			<a class="midLink">Matemaatiline Analüüs</a>
+		</div>
+		<div class="headRight">
+			<a class="midLink">Search</a>
+		</div>
+							
+			
 	</div><br></br>		
+	<div class="postBoxRow">
+		<div class="vertIcon">
+		</div>
+		<div class="postBox">	
+			<div class="voteBox">
+				<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="img/upV.png" width="50" height="50" alt="" /></a>
+				
+			</div>
+			<div class="postDataBox">	
+				<div class="postDataBoxUp">	
+					<a class="s1" href="#4">Kat?</a>
+					
+				</div>
+				<div class="postDataBoxDown">
+					<div class="dataComments">	
+						<h>Replies: 16</h>
+						
+					</div>
+					<div class="dataScore">	
+						<i1>78</i1>
+						
+					</div>				
+					<div class="dataDate">	
+						<h>16.02.2015</h>
+						
+					</div>
+				</div>			
+			</div>			
+			<div class="voteBox">
+				<a class="s1" href="http://www.reddit.com/"><img class="mid" src="img/downV.png" width="50" height="50" alt=""/></a>
+				
+			</div>				
+		</div>	
+	</div>
+	<div class="postBoxRow">
+		<div class="vertIcon">
+		</div>
+		<div class="postBox">	
+			<div class="voteBox">
+				<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="img/upV.png" width="50" height="50" alt="" /></a>
+				
+			</div>
+			<div class="postDataBox">	
+				<div class="postDataBoxUp">	
+					<a class="s1" href="#4">Kat?</a>
+					
+				</div>
+				<div class="postDataBoxDown">
+					<div class="dataComments">	
+						<h>Replies: 16</h>
+						
+					</div>
+					<div class="dataScore">	
+						<i1>78</i1>
+						
+					</div>				
+					<div class="dataDate">	
+						<h>16.02.2015</h>
+						
+					</div>
+				</div>			
+			</div>			
+			<div class="voteBox">
+				<a class="s1" href="http://www.reddit.com/"><img class="mid" src="img/downV.png" width="50" height="50" alt=""/></a>
+				
+			</div>				
+		</div>	
+	</div>
+	<div class="postBoxRow">
+		<div class="vertIcon">
+		</div>
+		<div class="postBox">	
+			<div class="voteBox">
+				<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="img/upV.png" width="50" height="50" alt="" /></a>
+				
+			</div>
+			<div class="postDataBox">	
+				<div class="postDataBoxUp">	
+					<a class="s1" href="#4">Kat?</a>
+					
+				</div>
+				<div class="postDataBoxDown">
+					<div class="dataComments">	
+						<h>Replies: 16</h>
+						
+					</div>
+					<div class="dataScore">	
+						<i1>78</i1>
+						
+					</div>				
+					<div class="dataDate">	
+						<h>16.02.2015</h>
+						
+					</div>
+				</div>			
+			</div>			
+			<div class="voteBox">
+				<a class="s1" href="http://www.reddit.com/"><img class="mid" src="img/downV.png" width="50" height="50" alt=""/></a>
+				
+			</div>				
+		</div>	
+	</div>
+	<div class="postBoxRow">
+		<div class="vertIcon">
+		</div>
+		<div class="postBox">	
+			<div class="voteBox">
+				<a class="s1" href="http://www.reddit.com/"><img class="mid"" src="img/upV.png" width="50" height="50" alt="" /></a>
+				
+			</div>
+			<div class="postDataBox">	
+				<div class="postDataBoxUp">	
+					<a class="s1" href="#4">Kat?</a>
+					
+				</div>
+				<div class="postDataBoxDown">
+					<div class="dataComments">	
+						<h>Replies: 16</h>
+						
+					</div>
+					<div class="dataScore">	
+						<i1>78</i1>
+						
+					</div>				
+					<div class="dataDate">	
+						<h>16.02.2015</h>
+						
+					</div>
+				</div>			
+			</div>			
+			<div class="voteBox">
+				<a class="s1" href="http://www.reddit.com/"><img class="mid" src="img/downV.png" width="50" height="50" alt=""/></a>
+				
+			</div>				
+		</div>	
+	</div>
 	<div class="postBoxRow">
 		<div class="vertIcon">
 		</div>
