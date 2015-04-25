10b47881642a8cd2b64bb9cdab3cb47c23a8cd59
priitkalda <priidrik@hotmail.com>
2015-04-25 23:36:43 +0300

    css fix

--- drawPosts.php
+++ drawPosts.php
@@ -4,11 +4,21 @@
 		include_once 'db/sql_functions.php';
 		
 		$data = getAllPosts($_POST['lecture'], "");
+		
 		foreach($data as $row){
 			 
 			$post = new Post($row['ID'], $row['Heading'], $_POST['lecture'], $row['Posted'], $row['Upvote']-$row['Downvote']);
 			$post->draw_post();
 			
 		}
+		
+		// for(int $i = 0; $i>10; $i++){
+			 
+			// $post = new Post($data[$i]['ID'], $data[$i]['Heading'], $_POST['lecture'], $data[$i]['Posted'], $data[$i]['Upvote']-$data[$i]['Downvote']);
+			// $post->draw_post();
+			
+		// }
+		
+		
 	}
 ?>
